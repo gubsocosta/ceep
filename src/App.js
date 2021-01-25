0058d29b289ac869a82fd82d345d2be8a0eba495
Gabriel Costa <gubsocosta@gmail.com>
2021-01-24 21:53:05 -0300

    chore: add eslint

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import RegisterForm from './components/RegisterForm';
 import './assets/index.css';
 import './assets/App.css';
 
-export default class App extends Component{
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      list: []
+      list: [],
     };
   }
 
   createCard(title, body) {
-    const newCard = {title, body};
-    const newList = [...this.state.list, newCard]
-    
+    const newCard = { title, body };
+    // eslint-disable-next-line react/destructuring-assignment
+    const newList = [...this.state.list, newCard];
+
     const newState = {
-      list: newList
+      list: newList,
     };
 
     this.setState(newState);
@@ -26,9 +27,12 @@ export default class App extends Component{
 
   render() {
     return (
+      // eslint-disable-next-line react/jsx-filename-extension
       <section className="content">
-        <RegisterForm createCard={ this.createCard.bind(this) }/>
-        <CardList list={ this.state.list } />
+        {/* eslint-disable-next-line react/jsx-no-bind */}
+        <RegisterForm createCard={this.createCard.bind(this)} />
+        {/* eslint-disable-next-line react/destructuring-assignment */}
+        <CardList list={this.state.list} />
       </section>
     );
   }
